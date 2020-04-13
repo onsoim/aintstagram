266d705892150a2bea9345470c634422e5373a58
wizleysw <wizley@kakao.com>
2020-04-13 20:27:44 +0900

    add gallery intent to MainActivity
    Reviewed by Wizley.
    2020.04.13.
    
    I have added ACTION_GET_CONTENT of images from device using intent.

--- app/src/main/java/com/ssg/aintstagram/MainActivity.java
+++ app/src/main/java/com/ssg/aintstagram/MainActivity.java
@@ -3,19 +3,30 @@ package com.ssg.aintstagram;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity{
     private static final int REQUEST_IMAGE_CAPTRUE = 1;
+    private static final int REQUEST_TAKE_ALBUM = 2;
     private ImageButton btn_camera;
+    private ImageButton btn_add;
+
+
+    String[] PERMISSIONS = {
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +37,55 @@ public class MainActivity extends AppCompatActivity{
 
     }
 
-    public void setBtn(){
-        btn_camera = (ImageButton)findViewById(R.id.button_to_camera);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTRUE && resultCode == RESULT_OK && data.hasExtra("data")) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
 
-        View.OnClickListener Listener = new View.OnClickListener(){
+            // bitmap 처리 부분
+        }
+    }
+
+    public void setBtn() {
+        btn_camera = (ImageButton) findViewById(R.id.button_to_camera);
+        btn_add = (ImageButton) findViewById(R.id.button_to_add);
+
+        View.OnClickListener Listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int permissionCheck;
                 switch (v.getId()) {
                     case R.id.button_to_camera:
-                        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
-                        if(permissionCheck == PackageManager.PERMISSION_DENIED){
-                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},0);
+                        permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+                        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, 0);
                         } else {
-                            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTRUE);
+                            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            if (intent.resolveActivity(getPackageManager()) != null) {
+                                startActivityForResult(intent, REQUEST_IMAGE_CAPTRUE);
                             }
                         }
                         break;
-                    case R.id.button_to_home:
+
+                    case R.id.button_to_add:
+                        permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE);
+                        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+                            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS, 0);
+                        } else {
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_GET_CONTENT);
+                            intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            intent.setType("image/*");
+                            startActivityForResult(intent, REQUEST_TAKE_ALBUM);
+                        }
                         break;
                 }
             }
         };
 
         btn_camera.setOnClickListener(Listener);
+        btn_add.setOnClickListener(Listener);
     }
 }
+
