2b9831cf53aa6f23fb1c6bccdef99a145abba056
wizleysw <wizley@kakao.com>
2020-04-13 16:19:17 +0900

    add Imagebuttons and takePictureIntent to handle camera btn
    Reviewed by Wizley.
    2020.04.13.
    
    I have changed all buttons of main.xml to imagebutton. In MainActivity, I have added setBtn to handle button initialization and Listener to handle onClick overriding methods of thouse imagebuttons. If we do not check permission app will be shutdown, so added requestPermissions to check whether user granted permission.

--- app/src/main/java/com/ssg/aintstagram/MainActivity.java
+++ app/src/main/java/com/ssg/aintstagram/MainActivity.java
@@ -1,13 +1,55 @@
 package com.ssg.aintstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+    private static final int REQUEST_IMAGE_CAPTRUE = 1;
+    private ImageButton btn_camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.setBtn();
+
+    }
+
+    public void setBtn(){
+        btn_camera = (ImageButton)findViewById(R.id.button_to_camera);
+
+        View.OnClickListener Listener = new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button_to_camera:
+                        int permissionCheck = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+                        if(permissionCheck == PackageManager.PERMISSION_DENIED){
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},0);
+                        } else {
+                            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTRUE);
+                            }
+                        }
+                        break;
+                    case R.id.button_to_home:
+                        break;
+                }
+            }
+        };
+
+        btn_camera.setOnClickListener(Listener);
     }
 }
